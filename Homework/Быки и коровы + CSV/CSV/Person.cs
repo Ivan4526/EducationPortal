3d33f6b0432e2566ae75e3f8d7a2844935fe748c
Ivan <Ivan4526@users.noreply.github.com>
2021-01-11 18:23:51 +0200

    Bulls and cows + CSV

---
+++ Homework/Быки и коровы + CSV/CSV/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace CSV
+{
+    class Person
+    {
+        public object this[string propertyName]
+        {
+            get
+            {
+                Type myType = typeof(Person);
+                PropertyInfo myPropInfo = myType.GetProperty(propertyName);
+                return myPropInfo.GetValue(this, null);
+            }
+        }
+        public int Age { get; set; }
+        public string EyeColor { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string Company { get; set; }
+        public string Address { get; set; }
+        public decimal? Salary { get; set; }
+
+    }
+}
---
+++ Homework/Быки и коровы + CSV/CSV/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace CSV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameOfFields = Console.ReadLine();
+            string[] fields = nameOfFields.Split(',');
+            var personList = PersonList.GetListPerson();
+            StringBuilder builder = new StringBuilder();
+            foreach (var person in personList)
+            {
+                for (int q = 0; q < fields.Length; q++)
+                {
+                    builder.Append($"{person[fields[q]]}\n");
+                }
+            }
+            string filePath = @"D:\test.csv";
+            File.WriteAllText(filePath, builder.ToString());
+
+        }
+    }
+}
