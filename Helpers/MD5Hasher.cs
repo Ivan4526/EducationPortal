7ed7190949278f9d3f0cb84af99b594d2da8e024
Ivan <Ivan4526@users.noreply.github.com>
2021-02-10 15:08:58 +0200

    Created new abstraction layer (Helpers)

---
+++ Helpers/MD5Hasher.cs
@@ -0,0 +1,20 @@
+﻿using Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Helpers
+{
+    public class MD5Hasher : IPasswordHasher
+    {
+        public string ComputeHash(string password)
+        {
+            using (var md5 = new MD5CryptoServiceProvider())
+            {
+                byte[] data = md5.ComputeHash(Encoding.UTF8.GetBytes(password));
+                return Convert.ToBase64String(data);
+            }
+        }
+    }
+}
