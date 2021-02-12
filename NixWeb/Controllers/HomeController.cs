6fcc7a1867a3c8a9f66e3ca84e2eb7f83b7fdaea
Ivan <Ivan4526@users.noreply.github.com>
2021-02-12 17:49:53 +0200

    Created ASP.NET Core project

---
+++ NixWeb/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using Service;
+using Interfaces;
+using Models;
+
+namespace NixWeb.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        ILogger<HomeController> _logger;
+        IUserService service; 
+
+        public HomeController(ILogger<HomeController> logger, IUserService service)
+        {
+            _logger = logger;
+            this.service = service;
+        }
+
+        [HttpGet]
+        public void Add(User user)
+        {
+            service.CreateUser(user);
+        }
+    }
+}
