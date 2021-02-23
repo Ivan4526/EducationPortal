92987f7b3d443735d393aae676e562a9949a37b8
Ivan <Ivan4526@users.noreply.github.com>
2021-02-23 13:20:41 +0200

    setup startup file

--- NixWeb/Startup.cs
+++ NixWeb/Startup.cs
@@ -1,10 +1,18 @@
+using Helpers;
+using Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using Models;
+using Repository;
+using Service;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,7 +32,38 @@ namespace NixWeb
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<Context>(
+            options => options.UseSqlServer(Configuration.GetConnectionString("MainConnection")), ServiceLifetime.Transient);
+
+            services.AddScoped<IRepository<User>, EntityRepository<User>>();
+            services.AddScoped<IRepository<Course>, EntityRepository<Course>>();
+            services.AddScoped<IRepository<Role>, EntityRepository<Role>>();
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<ICourseService, CourseService>();
+            services.AddSingleton<IPasswordHasher, Helpers.MD5Hasher>();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                     .AddJwtBearer(options =>
+                     {
+                         options.RequireHttpsMetadata = false;
+                         options.TokenValidationParameters = new TokenValidationParameters
+                         {
+                             ValidateIssuer = true,
+                             ValidIssuer = AuthOptions.ISSUER,
+                             ValidateAudience = true,
+                             ValidAudience = AuthOptions.AUDIENCE,
+                             ValidateLifetime = true,
+                             IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
+                             ValidateIssuerSigningKey = true,
+                         };
+                     });
+
             services.AddControllers();
+
+            services.AddCors(c =>
+            {
+                c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,10 +73,15 @@ namespace NixWeb
             {
                 app.UseDeveloperExceptionPage();
             }
-
+        
             app.UseRouting();
 
+            app.UseCors("AllowOrigin");
+
+            app.UseAuthentication();
+
             app.UseAuthorization();
+
 
             app.UseEndpoints(endpoints =>
             {
