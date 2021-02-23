a386fe32c50828e1bb725c97b894f565925ede17
Ivan <Ivan4526@users.noreply.github.com>
2021-02-23 13:21:26 +0200

    Updaed abstraction layer "Helpers"

---
+++ Helpers/Mapers/MaterialMaper.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Helpers.Mapers
+{
+    public class MaterialMaper
+    {
+        public MaterialMaper(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Material>()
+.HasOne(s => s.Video)
+.WithOne(ad => ad.Material)
+.HasForeignKey<Video>(ad => ad.MaterialId);
+
+            modelBuilder.Entity<Material>()
+.HasOne(s => s.Article)
+.WithOne(ad => ad.Material)
+.HasForeignKey<Article>(ad => ad.MaterialId);
+
+            modelBuilder.Entity<Material>()
+.HasOne(s => s.Book)
+.WithOne(ad => ad.Material)
+.HasForeignKey<Book>(ad => ad.MaterialId);
+        }
+    }
+}
---
+++ Helpers/Mapers/UserMaper.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Helpers.Mapers
+{
+    public class UserMaper
+    {
+        public UserMaper(ModelBuilder modelBuilder)
+        {
+          /* modelBuilder.Entity<User>()
+          .HasOne(s => s.Role)
+          .WithMany(g => g.Users)
+          .HasForeignKey(s => s.RoleId);
+
+           modelBuilder.Entity<UserSkills>().HasKey(sc => new { sc.SkillId, sc.UserId });
+
+           modelBuilder.Entity<UserCourses>().HasKey(sc => new { sc.CourseId, sc.UserId });*/
+        }
+    }
+}
