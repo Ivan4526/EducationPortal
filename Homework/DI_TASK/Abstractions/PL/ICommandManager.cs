6d2c0e0d363cd519106ed92a01416eb69b0a723f
Ivan <Ivan4526@users.noreply.github.com>
2021-03-09 14:23:15 +0200

    DI_Homework

---
+++ Homework/DI_TASK/Abstractions/PL/ICommandManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DI.App.Abstractions.PL
+{
+    public interface ICommandManager
+    {
+         void Start();
+    }
+}
---
+++ Homework/DI_TASK/MyDI.cs
@@ -0,0 +1,25 @@
+﻿using DI.App.Abstractions;
+using DI.App.Abstractions.BLL;
+using DI.App.Abstractions.PL;
+using DI.App.Models;
+using DI.App.Services;
+using DI.App.Services.PL;
+using DI.App.Services.PL.Commands;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DI.App
+{
+    public static  class MyDI
+    {
+        public static IServiceProvider SetupServices()
+           => new ServiceCollection()
+            .AddSingleton<IUserStore, UserStore>()
+            .AddSingleton<ICommandProcessor, CommandProcessor>()
+            .AddSingleton<ICommand, AddUserCommand>()
+            .AddSingleton<IDatabaseService, InMemoryDatabaseService>()
+            .BuildServiceProvider();
+    }
+}
